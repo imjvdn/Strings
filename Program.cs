3387ee9a11ad4741c3bb91c26d78972a0092d588
Jadan morrow <jadanmorrow@outlook.com>
2020-08-07 07:31:56 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Jadan Morrow";
+            Console.WriteLine(phrase.IndexOf("Jadan MOrrow"));
+            
+            Console.ReadLine();
+
+        }
+    }
+}
